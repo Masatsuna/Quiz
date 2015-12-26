bf22f486ca232a8f40076b875f941c1e57027756
Masatsuna <CloQuespayl6>
2015-12-26 23:06:14 +0900

    問題文と注釈を追加

--- app/src/main/java/org/t_robop/masatsuna/quiz/FinalResult.java
+++ app/src/main/java/org/t_robop/masatsuna/quiz/FinalResult.java
@@ -18,15 +18,20 @@ public class FinalResult extends AppCompatActivity {
     public void onStart(){
         super.onStart();
 
+        //最終的な問題数と得点を表示
         TextView fin = (TextView) findViewById(R.id.finalResult);
         fin.setText("あなたの得点は" + QuizScreen.sentence.length + "点満点の" +
                 QuizScreen.getPoint() + "点でした。");
     }
 
+    //スタート画面に戻るボタンを押したときの実行内容
     public void onClick(View view){
+
+        //問題数のカウントと得点のカウントをリセット
         QuizScreen.setQuizNumber(0);
         QuizScreen.setPoint(0);
 
+        //StartScreenに移行
         Intent intent = new Intent(FinalResult.this, StartScreen.class);
         startActivity(intent);
     }
